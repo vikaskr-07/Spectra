c8f47f2359d9a985d0edae03a88da46213c56a22
Vikas Kumar <vikaskum7ar@gmail.com>
2024-10-23 00:09:28 +0530

    Update logo.tsx

--- app/(auth)/_components/logo.tsx
+++ app/(auth)/_components/logo.tsx
@@ -14,7 +14,7 @@ export function Logo() {
 				<Image src='/spooky.svg' alt='Gamehub' height='80' width='80' />
 			</div>
 			<div className={cn('flex flex-col items-center', font.className)}>
-				<p className='text-xl font-semibold'>Gamehub</p>
+				<p className='text-xl font-semibold'>Spectra</p>
 				<p className='text-sm text-muted-foreground'>Let&apos;s play</p>
 			</div>
 		</div>
